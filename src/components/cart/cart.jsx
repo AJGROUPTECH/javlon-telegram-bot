6539622c5331b6bd90a542c5f1277ac1eeac326c
Amirkulov Javlon <110676477+AJGROUPTECH@users.noreply.github.com>
2025-09-23 11:02:31 +0500

    Update cart.jsx

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -18,7 +18,7 @@ const Cart = ({ cartItems, onCheckout}) => {
      </p>
       <Button  title={`${cartItems.length === 0 ? "Buyurtma berish" : "To'lov"}`}
       disable={cartItems.length === 0 ? true : false} type={"checkout"} 
-      onClick={'onCheckout'}
+      onClick={onCheckout}
       
       />
      </div>
